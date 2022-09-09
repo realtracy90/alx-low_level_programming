2cf7e19ba7d8f487aa86100f5c1e043a883f1f01
realtracy90 <missrashee14@gmail.com>
2022-09-09 03:36:40 -0700

    rashida

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 
 /**
+ 
  * main - Entry point
     
  * Return: Always 0 (Success)
@@ -12,7 +13,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 	
 		return (0);
 
