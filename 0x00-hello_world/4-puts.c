9d9469fbc6237c9018ee50c738b6fc738f88f646
realtracy90 <missrashee14@gmail.com>
2022-09-09 03:46:00 -0700

    rashida

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,8 @@
 /**
  
  * main - Entry point
+ 
+ *
     
  * Return: Always 0 (Success)
  
@@ -13,7 +15,7 @@ int main(void)
 
 {
 
-        puts("\"Programming is like building a multilingual puzzle");
+        puts("/"Programming is like building a multilingual puzzle");
 	
 		return (0);
 
