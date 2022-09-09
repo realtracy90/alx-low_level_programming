7bee56f2a635031d0928baf1968d29b5cbe38ab1
realtracy90 <missrashee14@gmail.com>
2022-09-09 13:10:14 -0700

    rashida

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -16,12 +16,12 @@ int main(void)
 
 
 
-			srand(time(0));
+		srand(time(0));
 
-				n = rand() - RAND_MAX / 2;
+		n = rand() - RAND_MAX / 2;
 
-					/* your code goes there */
+		/* your code goes there */
 
-					return (0);
+		return (0);
 
 }
