42e95df721ec2e897d362dc6d5e356456af8ac0b
realtracy90 <missrashee14@gmail.com>
2022-09-13 08:56:12 -0700

    rashida

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,20 +14,4 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-}#include "main.h"
-
-/**
- * print_alphabet - Prints the alphabet in lowercase.
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
 }
