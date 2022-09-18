699dacd6b6634eb7d8ecdeb8c3f0b62be2919fa2
realtracy90 <missrashee14@gmail.com>
2022-09-18 12:26:12 -0700

    rashida

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,18 @@
-#include "main.h"
+#ifndef _main_h_
+#define _main_h_
 
-/**
- * main - Prints _putchar as a message.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
-	return (0);
-}
+#endif
